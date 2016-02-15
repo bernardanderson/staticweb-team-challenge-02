ca019b95e9c54cec69c58c528bf1c88fcb42a156
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-15 00:23:01 -0600

    Completed boxes and color addition, also dynamically adding initial color and select border

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
+var mainDiv = document.getElementById("main-div");
+var isFormVisible = document.getElementsByTagName('input');
 var divs = document.getElementsByClassName("flex-div");
-var formDiv = document.getElementById("form-div");
 
+// Thi
 function addInitialColor() {
   for (var i = 0; i < divs.length; i++) {
-    divs[i].classList.add("color"+i);
+    divs[i].style.background = "gray";
   }
 }
 
@@ -17,18 +19,40 @@ function addBorderRevealForm() {
   for (var i = 0; i < divs.length; i++) {
     if (divs[i].classList.contains("selected")) {
       divs[i].classList.remove("selected");
+      divs[i].style.border = "";
+      break;
     }
   }
   this.classList.add("selected");
-  formDiv.classList.remove("hidden");
+  this.style.border = "2px solid red";
   buildForm();
 }
 
 function buildForm() {
-  
+  if (isFormVisible.length === 0) {
+    var formDiv = document.createElement("div");
+    formDiv.classList.add("form-div")
+    formDiv.innerHTML = "<input type='text' id='input-color' placeholder='Enter a color'>";
+    formDiv.innerHTML += "<p id='button'>COLOR IT</p>";
+    mainDiv.appendChild(formDiv);
+    checkForColorClick();
+  }
 }
 
+function checkForColorClick() {
+  var button = document.getElementById("button");
+  button.addEventListener("click", insertColor);
+}
+
+function insertColor() {
+  var bgColor = document.getElementById("input-color");
+  for (var i = 0; i < divs.length; i++) {
+    if (divs[i].classList.contains("selected")) {
+      divs[i].style.background = bgColor.value;
+      break;
+    }
+  }
+}
 
 addInitialColor();
 addDivListeners();
-
